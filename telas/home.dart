fc72cca478fd5f4c0355b6d45febe915bac7a4f1
Abreu <felipe.abreu@accenture.com>
2020-03-12 11:08:54 -0300

    Se aplicado, este commit irá criar o tema, as abas e a interface com gridview

---
+++ telas/home.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'bichos.dart';
+import 'numeros.dart';
+import 'vogais.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Aprenda inglês"),
+        bottom: TabBar(
+          indicatorWeight: 4,
+          indicatorColor: Colors.white,
+          labelStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          controller: _tabController,
+          tabs: <Widget>[
+            Tab(
+              text: "Bichos",
+            ),
+            Tab(
+              text: "Números",
+            ),
+            Tab(
+              text: "Vogais",
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Bichos(),
+          Numeros(),
+          Vogais(),
+        ],
+      ),
+    );
+  }
+}
