a619445710af45fef9424579bdf252c955becc28
Abreu <felipe.abreu@accenture.com>
2020-03-16 18:13:29 -0300

    Se aplicado, este commit irá corrigir o problema da exibição das telas

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_aprenda_ingles/telas/home.dart';
 
-void main() => runApp(MaterialApp(
-  home: Home(),
-  debugShowCheckedModeBanner: false,
-  theme: ThemeData(
-    primaryColor: Color(0xff795548),
-    scaffoldBackgroundColor: Color(0xfff5e9b9)
-  ),
-));
-
-class Home extends StatefulWidget {
-  Home({Key key}) : super(key: key);
-
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Container(),
-    );
-  }
+void main() {
+  runApp(
+    MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff795548),
+        scaffoldBackgroundColor: Color(0xfff5e9b9),
+      ),
+    ),
+  );
 }
